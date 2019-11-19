8485bc3fb5d434426f4e8f8229a013a29ada917a
Joshua Peters <peters.l.joshua@gmail.com>
2019-11-18 23:34:53 -0600

    saving personal website progress

--- website.js
+++ website.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     "use strict";
+
 $('.navbar-brand').css('color','white');
 
 });
 
+$('#test-button').on('click', function(){
+    if(!$('.navbar-collapse').hasClass('show')){
+        $('.navbar-toggler').collapse('hide');
+    }
+});

bf4bd4c58975330a28d1fc07c9f8f5b24584f201
Joshua Peters <peters.l.joshua@gmail.com>
2020-01-27 23:41:09 -0600

    saving jquery button back to top complete

--- website.js
+++ website.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
     "use strict";
 
-// $('.navbar-brand').css('color','white');
 
     $('.dropdown-item').on('click',function (){
         $('.navbar-collapse').collapse('hide');
     });
+
+    $('#myBtn').on('click',function () {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+        // document.body.scrollTop = 0; // For Safari
+        // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
+    //Get the button:
+    let mybutton;
+    mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+
 
 
 

6fa2c2a5dc4e953408a06b49cfafde094c2931f0
Joshua Peters <peters.l.joshua@gmail.com>
2020-02-05 16:13:49 -0600

    fixed react button for mobile; kinda haha XD

--- like_button.js
+++ like_button.js
@@ -21,8 +21,32 @@ class LikeButton extends React.Component {
         );
     }
 }
+// mobile
+
+class LikeButtonTwo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
 
 // ... the starter code you pasted ...
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
+
+const domContainerTwo = document.querySelector('#like_button_container_two');
+ReactDOM.render(e(LikeButtonTwo), domContainerTwo);
